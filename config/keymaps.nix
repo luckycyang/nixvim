0f12d35dbff990a704c104b29015c8735cde3913
dingduck <kfove.liang@gmail.com>
2024-04-18 00:35:24 +0800

    dap && rustaceanvim

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -9,5 +9,43 @@
         desc = "quit";
       };
     }
+    {
+      mode = "n";
+      key = "<C-k>";
+      action = "<C-W>k";
+      options = {
+        silent = true;
+        desc = "Move to window over";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-j>";
+      action = "<C-W>j";
+      options = {
+        silent = true;
+        desc = "Move to window bellow";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-h>";
+      action = "<C-W>h";
+      options = {
+        silent = true;
+        desc = "Move to window left";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-l>";
+      action = "<C-W>l";
+      options = {
+        silent = true;
+        desc = "Move to window right";
+      };
+    }
   ];
 }
--- config/plugs/lsp/dap.nix
+++ config/plugs/lsp/dap.nix
@@ -1,11 +1,52 @@
-{
+{pkgs, ...}: {
   plugins.dap = {
     enable = true;
+    configurations = {
+      cpp = [
+        {
+          name = "Lauch cpp";
+          request = "launch";
+          type = "gdb";
+          program = ''
+            function()
+            	return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end
+          '';
+          cwd = "\${workspaceFolder}";
+          stopAtEntry = true;
+        }
+      ];
+    };
+
+    adapters.executables = {
+      gdb = {
+        args = ["-i" "dap"];
+        command = "${pkgs.gdb}/bin/gdb";
+      };
+    };
     extensions.dap-ui = {
       enable = true;
       mappings = {
-        toggle = "<leader>gt";
+        toggle = "t";
       };
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>du";
+      action = "<cmd>lua require('dapui').toggle()<CR>";
+      options = {
+        desc = "Dapui toggle";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>de";
+      action = "<cmd>lua require('dapui').eval()<CR>";
+      options = {
+        desc = "Dapui Eval";
+      };
+    }
+  ];
 }
