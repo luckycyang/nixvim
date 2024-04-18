33fe49bfeab6765c06426c86dd23adf250cbe9e8
dingduck <kfove.liang@gmail.com>
2024-04-18 15:25:05 +0800

    stub dap

--- config/plugs/lsp/dap.nix
+++ config/plugs/lsp/dap.nix
@@ -1,27 +1,22 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  workdir = "$" ++ "{workspaceFolder}";
+in {
   plugins.dap = {
     enable = true;
-    configurations = {
-      c = [
-        {
-          name = "Launch";
-          type = "gdb";
-          request = "launch";
-          program = ''              
-            function()
-              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-            end'';
-          cwd = "\${workspaceFolder}";
-          stopAtBeginningOfMainSubprogram = false;
-        }
-      ];
-    };
-
     adapters.executables = {
       gdb = {
         args = ["-i" "dap"];
         command = "${pkgs.gdb}/bin/gdb";
       };
+    };
+    configurations = {
+      cpp = [
+        {
+          name = "cpp";
+          request = "launch";
+          type = "gdb";
+        }
+      ];
     };
     extensions = {
       dap-ui = {
@@ -66,18 +61,5 @@
     }
   ];
   extraConfigLua = ''
-    local dap = require("dap")
-    dap.configurations.cpp = {
-      {
-        name = "Launch",
-        type = "gdb",
-        request = "launch",
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = "$\{workspaceFolder}",
-        stopAtBeginningOfMainSubprogram = false,
-      },
-    }
   '';
 }
