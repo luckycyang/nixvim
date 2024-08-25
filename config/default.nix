41f0e4f202150c72e7e940d7d381a01cc78e2c01
luckycyang <kfove.liang@protonmail.com>
2024-08-25 21:12:11 +0800

    simpe setup

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,11 @@
 {
   # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+  imports = [ 
+    ./bufferline.nix
+    ./sets.nix
+    ./plug/colortheme
+    ./plug/completion/cmp.nix
+    ./plug/completion/lspkind.nix
+    ./plug/snippets/luasnip.nix
+  ];
 }
