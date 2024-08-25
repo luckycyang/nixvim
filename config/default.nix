1cc4477bc871b60a6369db69f4855edd4b3b96a3
luckycyang <kfove.liang@protonmail.com>
2024-08-25 21:30:59 +0800

    simple add

--- config/default.nix
+++ config/default.nix
@@ -4,12 +4,20 @@
     ./bufferline.nix
     ./sets.nix
     ./plug/colortheme
+    # completion
     ./plug/completion/cmp.nix
     ./plug/completion/lspkind.nix
+    # snippets
     ./plug/snippets/luasnip.nix
     # lsp
     ./plug/lsp/lsp.nix
     ./plug/lsp/fidget.nix
     ./plug/lsp/hlchunk.nix
+
+    # utils
+    ./plug/utils/whichkey.nix
+
+    # treesitter
+    ./plug/treesitter/treesitter.nix
   ];
 }
